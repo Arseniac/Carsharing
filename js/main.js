4b2025363786269b63e0fe8bdba57b2e888c5343
Arseniac <a2611k@gmail.com>
2021-08-19 01:55:26 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+    });
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
+
+});
